a15c1e09cfda0c5677f396879492e5484e24ee22
Muhammad Raflan Kemal <raflankemal05@gmail.com>
2024-11-03 21:29:47 +0700

    Add files via upload

---
+++ Muhammad Raflan Kemal_2311103132_Modul 3/LatihanP7RaflanKemal/src/latihanp7raflankemal/KRS.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package latihanp7raflankemal;
+
+/**
+ *
+ * @author Muhammad Raflan Kemal
+ * 2311103132
+ * 07C
+ */
+interface KRS {
+
+    void tambahMatakuliah(MataKuliah mk);
+
+    void hapusMatakuliah(MataKuliah mk);
+}
